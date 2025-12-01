f8904bcc590add015c7dc2c3ccbbb63e2077f788
aswathkumar24 <aswathkumar18@gmail.com>
2025-12-01 22:26:01 +0530

    Create Program019.c

---
+++ Program019.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Write a C program that accepts 4 integers p, q, r, s from the user where q, r and s are positive 
+and p is even. If q is greater than r and s is greater than p and if the sum of r and s is greater
+than the sum of p and q print "Correct values", otherwise print "Wrong values".
+Test Data :
+Input the second integer: 35
+Input the third integer: 15
+Input the fourth integer: 46
+Expected Output:
+Wrong values
+
+*******************************************************************************/
+#include <stdio.h>  //program 19
+
+int main()
+{
+    unsigned int q,r,s;
+    int p;
+    printf("Input the first integer: ");
+    scanf("%d",&p);
+    printf("Input the second integer: ");
+    scanf("%d",&q);
+    printf("Input the third integer: ");
+    scanf("%d",&r);
+    printf("Input the fourth integer: ");
+    scanf("%d",&s);
+    
+    if((q>0)&&(r>0)&&(s>0)&&(p%2==0)&&(q>r)&&(s>p)&&((r+s)>(p+q)))
+        printf("Correct Values");
+    else
+        printf("Wrong Values");
+    return 0;
+}
