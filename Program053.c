d94b90260245ef3b6b77a559ea7f5a2a0ddb1928
aswathkumar24 <aswathkumar18@gmail.com>
2025-12-03 09:38:53 +0530

    Create Program053.c

---
+++ Program053.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Write a C program that accepts the principle, rate of interest, and time and calculates simple interest.
+Test Data:
+Input Data: p = 10000, r = 10% , t = 12 year
+Expected Output:
+Input principle, Rate of interest & time to find simple interest:
+Simple interest = 12000
+
+*******************************************************************************/
+#include <stdio.h>      //Program 53
+
+int si(int a, int b, int c)
+{
+    return ((a*b*c)/100);
+}
+
+int main()
+{
+    int p,r,t,interest;
+    printf("Principle: ");
+    scanf("%d",&p);
+    printf("Rate of interest: ");
+    scanf("%d",&r);
+    printf("Time: ");
+    scanf("%d",&t);
+    
+    interest = si(p,r,t);
+    printf("Simple interest = %d",interest);
+
+    return 0;
+}
