fd460769b65654a0ecdaa84118d6d0f6428e3622
aswathkumar24 <aswathkumar18@gmail.com>
2025-12-04 11:47:49 +0530

    Create Program076.c

---
+++ Program076.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Write a C program to calculate and print the squares and cubes of the numbers from 0 to 20.
+It uses tabs to display them in a table of values.
+Sample Output:
+Number Square Cube
+=========================
+0 0 0
+1 1 1
+2 4 8
+3 9 27
+.....
+18 324 5832
+19 361 6859
+20 400 8000
+
+*******************************************************************************/
+#include <stdio.h>      //Program 76
+
+int main()
+{
+    for (int i=0;i<21;i++)
+    {
+        printf("%d\t%d\t%d\n",i,i*i,i*i*i);
+    }
+        
+    
+}
